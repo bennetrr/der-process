b8bae0019327e55995f237280c7aa2b0ec7fb9ac
bennetr <76742318+bennetrr@users.noreply.github.com>
2022-10-11 19:32:19 +0200

    Verbesserungen

--- src/pages/entstehungskontext.tsx
+++ src/pages/entstehungskontext.tsx
@@ -13,7 +13,8 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <BackgroundImage uri={"/img/Der_Process_Manuskript.png"} desc={"Die erste Seite des Manuskriptes zu Der Process"}/>
+            <BackgroundImage uri={"/img/Der_Process_Manuskript.png"}
+                             desc={"Die erste Seite des Manuskriptes zu Der Process"}/>
             <Header/>
 
             <div className={styles.page_content}>
@@ -21,29 +22,34 @@ export default function Home() {
                     <h1>Entstehungskontext</h1>
                     <ul>
                         <li>
-                            <b>Ereignisse im Leben Kafkas:</b><br/>
-                            Kafka fängt im Sommer 1914 an, den Roman zu schreiben. Im Juli 1914 fand die Auflösung der
-                            Verlobung mit Felice Bauer statt, die den Roman maßgeblich prägte (siehe biografischer
-                            Interpretationsansatz). Außerdem wohnte Kafka ab Herbst das erste Mal unabhängig von seinen
-                            Eltern.
+                            <b>Ereignisse im Leben Kafkas:</b>
+                            <p className={styles.seamlessParagraph}>
+                                Kafka fängt im Sommer 1914 an, den Roman zu schreiben. Im Juli 1914 fand die Auflösung
+                                der Verlobung mit Felice Bauer statt, die den Roman maßgeblich prägte (siehe
+                                biografischer Interpretationsansatz). Außerdem wohnte Kafka ab Herbst das erste Mal
+                                unabhängig von seinen Eltern.
+                            </p>
                         </li>
-                        <br/>
                         <li>
-                            <b>1. Weltkrieg:</b><br/>
-                            Am 28. Juli 1914 erklärte Österreich-Ungarn Serbien den Krieg, aus dem der Erste Weltkrieg
-                            wurde. Kafka wurde wegen seiner wichtigen beruflichen Stellung im Gegensatz zu vielen
-                            anderen nicht eingezogen, was zu seiner Isolation beigetragen hat.
+                            <b>1. Weltkrieg:</b>
+                            <p className={styles.seamlessParagraph}>
+                                Am 28. Juli 1914 erklärte Österreich-Ungarn Serbien den Krieg, aus dem der Erste
+                                Weltkrieg wurde. Kafka wurde wegen seiner wichtigen beruflichen Stellung im Gegensatz zu
+                                vielen anderen nicht eingezogen, was zu seiner Isolation beigetragen hat.
+                            </p>
                         </li>
-                        <br/>
                         <li>
-                            <b>Fragment:</b><br/>
-                            Nach ein paar Monaten zog Kafka andere Werke dem Roman vor. Ab dann schrieb Kafka <i>Der
-                            Process</i> nicht-linear. Als Erstes wurden Anfangs- und Schlusskapitel geschrieben, an den
-                            anderen Kapiteln wurde parallel weitergearbeitet. Deshalb schrieb Kafka die Kapitel von <i>Der
-                            Process</i> auch in Hefte, in denen er auch andere Texte schrieb. Kafka legte keine
-                            bestimmte Reihenfolge der Kapitel fest, weshalb es mehrere Ausgaben mit unterschiedlichen
-                            Aufteilungen und Reihenfolgen gibt. Anfang 1915 brach Kafka die Arbeiten endgültig ab und
-                            ließ mehrere Kapitel unvollständig, die in manchen Ausgaben als Fragmente angehangen sind.
+                            <b>Fragment:</b>
+                            <p className={styles.seamlessParagraph}>
+                                Nach ein paar Monaten zog Kafka andere Werke dem Roman vor. Ab dann schrieb Kafka <i>Der
+                                Process</i> nicht-linear. Als Erstes wurden Anfangs- und Schlusskapitel geschrieben, an
+                                den anderen Kapiteln wurde parallel weitergearbeitet. Deshalb schrieb Kafka die Kapitel
+                                von <i>Der Process</i> auch in Hefte, in denen er auch andere Texte schrieb. Kafka legte
+                                keine bestimmte Reihenfolge der Kapitel fest, weshalb es mehrere Ausgaben mit
+                                unterschiedlichen Aufteilungen und Reihenfolgen gibt. Anfang 1915 brach Kafka die
+                                Arbeiten endgültig ab und ließ mehrere Kapitel unvollständig, die in manchen Ausgaben
+                                als Fragmente angehangen sind.
+                            </p>
                         </li>
                     </ul>
                 </div>
